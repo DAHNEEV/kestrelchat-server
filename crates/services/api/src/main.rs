dc49ef5cca3dad0990aa55129706e443d9a70982
striberry <stribes@tutanota.de>
2026-04-27 07:47:43 +0200

    chore: rename crate `rocket_okapi` to just `okapi` for ease of use
    Signed-off-by: striberry <stribes@tutanota.de>

--- crates/services/api/src/main.rs
+++ crates/services/api/src/main.rs
@@ -51,11 +51,10 @@ async fn rocket() -> _ {
         .await
         .expect("Failed to run database migrations");
 
-    let swagger =
-        rocket_okapi::swagger_ui::make_swagger_ui(&rocket_okapi::swagger_ui::SwaggerUIConfig {
-            url: "/openapi.json".to_owned(),
-            ..Default::default()
-        });
+    let swagger = okapi::swagger_ui::make_swagger_ui(&okapi::swagger_ui::SwaggerUIConfig {
+        url: "/openapi.json".to_owned(),
+        ..Default::default()
+    });
 
     let cors = CorsFairing {
         config: config.network.cors.clone(),
